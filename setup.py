e65b7271e8abbca0a912dcd40b5d763398da5c1b
Amir Shabani <amishbni@gmail.com>
2025-02-10 18:55:36 +0330

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="copyonwrite",
+    version="0.1.0",
+    description="Copy-on-Write for Python's mutable data structures",
+    author="Amir Shabani",
+    author_email="ami.shbni@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
